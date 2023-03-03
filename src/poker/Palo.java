b17996404129d5ef4fba9f8a030e629ca8abf624
Marcos B <marcosbailon@hotmail.com>
2023-03-03 20:34:30 +0100

    Primera subida

---
+++ src/poker/Palo.java
@@ -0,0 +1,14 @@
+package poker;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+
+/**
+ *
+ * @author MarcosB <your.name at your.org>
+ */
+public enum Palo {
+    
+}
---
+++ src/poker/Principal.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poker;
+
+/**
+ *
+ * @author MarcosB <your.name at your.org>
+ */
+public class Principal {
+    public static void main(String[] args) {
+        
+    }
+}
